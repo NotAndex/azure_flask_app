9bb7e4141dce3a16b3ab1d4aac7c7691224fb8b6
NotAndex <85022950+NotAndex@users.noreply.github.com>
2022-10-21 08:55:05 +0200

    sdfs

--- app.py
+++ app.py
@@ -6,26 +6,31 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    print("Request for index page received")
-    return render_template("index.html")
+def hello_world():
+    return f"<p>CWD = {Path.cwd()} Home = {Path.home()}</p>"
 
 
-@app.route("/favicon.ico")
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico", mimetype="image/vnd.microsoft.icon")
+# @app.route("/")
+# def index():
+#     print("Request for index page received")
+#     return render_template("index.html")
 
 
-@app.route("/hello", methods=["POST"])
-def hello():
-    name = request.form.get("name")
+# @app.route("/favicon.ico")
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico", mimetype="image/vnd.microsoft.icon")
 
-    if name:
-        print(f"{Path.cwd()}")
-        return render_template("hello.html", name=name)
-    else:
-        print("Request for hello page received with no name or blank name -- redirecting")
-        return redirect(url_for("index"))
+
+# @app.route("/hello", methods=["POST"])
+# def hello():
+#     name = request.form.get("name")
+
+#     if name:
+#         print(f"{Path.cwd()}")
+#         return render_template("hello.html", name=name)
+#     else:
+#         print("Request for hello page received with no name or blank name -- redirecting")
+#         return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
