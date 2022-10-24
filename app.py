e17a6c8c32b0f29f338396e08624ff2ed5210026
NotAndex <85022950+NotAndex@users.noreply.github.com>
2022-10-24 09:29:13 +0200

    azure

--- app.py
+++ app.py
@@ -1,55 +1,69 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, Response
 from pathlib import Path
-import base64
+import time
 from io import BytesIO
-from matplotlib.figure import Figure
-import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, DirCreatedEvent, FileCreatedEvent
 
 app = Flask(__name__)
-# p = Path("/mnt")
-# L = [x for x in p.iterdir() if x.is_dir()]
 
 
-# @app.route("/")
-# def hello():
-#     # Generate the figure **without using pyplot**.
-#     fig = Figure()
-#     ax = fig.subplots()
-#     ax.plot([1, 2])
-#     # Save it to a temporary buffer.
-#     buf = BytesIO()
-#     fig.savefig(buf, format="png")
-#     # Embed the result in the html output.
-#     data = base64.b64encode(buf.getbuffer()).decode("ascii")
-#     return f"<img src='data:image/png;base64,{data}'/>"
-# @app.route("/")
-# def index():
-#     return "Flask app"
+class CustomHandler(FileSystemEventHandler):
+    """Custom handler for Watchdog"""
+
+    def __init__(self):
+        # List to store path
+        self.path_strings = []
+
+    # callback for File/Directory created event, called by Observer.
+    def on_created(self, event: FileCreatedEvent):
+
+        self.path_strings.append(Path(event.src_path).as_posix())
+
+        print(f"Path content: \n{self.path_strings}")
 
 
-# @app.route("/")
-# def index():
-#     print("Request for index page received")
-#     return render_template("index.html")
+def main():
+    # get current path as absolute, linux-style path.
+    working_path = Path("/mnt/flask").as_posix()
+
+    # create instance of observer and CustomHandler
+    observer = Observer()
+    handler = CustomHandler()
+
+    # start observer, checks files recursively
+    observer.schedule(handler, path=working_path, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(5)
+            print(f"Image to yield == {len(handler.path_strings)}")
+            if len(handler.path_strings):
+                im = open(handler.path_strings.pop(), "rb").read()
+                print(f"After read {handler.path_strings}")
+                for i in range(2):  # IDK why this double yield is needed
+                    print(f"loop: {i}")
+                    yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + im + b"\r\n")
+
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+@app.route("/slideshow")
+def slideshow():
+
+    return Response(main(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 @app.route("/")
-def favicon():
-    return send_from_directory("/mnt/flask", "GitHub-Mark-64px.png")
-
-
-# @app.route("/hello", methods=["POST"])
-# def hello():
-#     name = request.form.get("name")
-
-#     if name:
-#         print(f"{Path.cwd()}")
-#         return render_template("hello.html", name=name)
-#     else:
-#         print("Request for hello page received with no name or blank name -- redirecting")
-#         return redirect(url_for("index"))
+def index():
+    return "<html><head></head><body><h1>slideshow</h1><img src='/slideshow' style='width: 90%; height: 90%;'/>" "</body></html>"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True)
+    # app.run()
