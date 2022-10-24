6f44b57693e31c7bbc719f5b9177808b42e4979e
NotAndex <85022950+NotAndex@users.noreply.github.com>
2022-10-24 12:33:03 +0200

    asd

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def main():
                 yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + im + b"\r\n")
                 yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + im + b"\r\n")
             else:
-                im = open("mnt/flask/inception.png", "rb").read()
+                im = open("/mnt/flask/inception.png", "rb").read()
                 # for i in range(2):  # IDK why this double yield is needed
                 #     print(f"loop: {i}")
                 yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + im + b"\r\n")
